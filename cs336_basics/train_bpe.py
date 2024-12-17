b2ef01e8c40e5dca985da7a4d06fc88ece8a5a54
Jack Gindi <gindije@gmail.com>
2024-12-17 08:33:30 -0500

    factored out merges and vocab

--- cs336_basics/train_bpe.py
+++ cs336_basics/train_bpe.py
@@ -3,19 +3,9 @@ import collections
 import json
 import regex as re
 import sys
-from typing import Dict, Generator, Iterable, List, Optional, Set, Tuple
+from typing import Dict, Iterable, List, Optional, Set, Tuple, Union
 
 import tqdm
-
-
-def get_int_pair_counts(
-    token_count_dict: Dict[Tuple, int]
-) -> Dict[Tuple[int, int], int]:
-    bp_counts = collections.defaultdict(int)
-    for w, wc in token_count_dict.items():
-        for pair in zip(w, w[1:]):
-            bp_counts[pair] += wc
-    return bp_counts
 
 
 def apply_merge_to_word(
@@ -52,6 +42,123 @@ def replace_pairs_by_location(
             new_word.append(word[i])
             i += 1
     return tuple(new_word)
+
+
+class Merge:
+
+    def __init__(self, left: Union[int, bytes], right: Union[int, bytes]):
+        tleft = type(left)
+        tright = type(right)
+        assert tleft == tright
+        assert tleft in {int, bytes}
+        self._left = left
+        self._right = right
+
+    @classmethod
+    def from_ints(cls, left: int, right: int) -> "Merge":
+        return cls(left, right)
+
+    @classmethod
+    def from_bytes(cls, left: bytes, right: bytes) -> "Merge":
+        return cls(left, right)
+
+    def as_int_tuple(
+        self, reverse_vocab: Optional[Dict[bytes, int]] = None
+    ) -> Tuple[int, int]:
+        if isinstance(self._left, int) and isinstance(self._right, int):
+            return (self._left, self._right)
+        assert reverse_vocab is not None
+        return (
+            reverse_vocab[self._left],
+            reverse_vocab[self._right],
+        )
+
+    def as_bytes_tuple(
+        self, vocab: Optional[Dict[int, bytes]] = None
+    ) -> Tuple[bytes, bytes]:
+        if isinstance(self._left, bytes) and isinstance(self._right, bytes):
+            return (self._left, self._right)
+        assert vocab is not None
+        return (vocab[self._left], vocab[self._right])
+
+
+def int2bytes(
+    ix: int, base: Dict[int, bytes], new: Dict[int, Tuple[int, int]]
+) -> bytes:
+    if ix in base:
+        return base[ix]
+    ix_left, ix_right = new[ix]
+    return int2bytes(ix_left, base, new) + int2bytes(ix_right, base, new)
+
+
+def pair2bytes(
+    pair: Tuple[int, int],
+    base: Dict[int, bytes],
+    new: Dict[int, Tuple[int, int]],
+) -> bytes:
+    return int2bytes(pair[0], base, new) + int2bytes(pair[1], base, new)
+
+
+class Vocab:
+
+    def __init__(self, vocab: Dict[int, bytes]):
+        self._forward_mapping = vocab
+        self._inverse_mapping = {v: k for k, v in vocab.items()}
+
+    def __getitem__(self, key: Union[int, bytes]):
+        if isinstance(key, bytes):
+            return self._inverse_mapping[key]
+        return self._forward_mapping[key]
+
+    def __len__(self):
+        return len(self._forward_mapping)
+
+    @classmethod
+    def from_base_and_new(
+        cls,
+        base_vocab: Dict[int, bytes],
+        new_vocab: Dict[int, Tuple[int, int]],
+        merges: List[Merge],
+    ) -> "Vocab":
+        reverse_new_vocab = {v: k for k, v in new_vocab.items()}
+        vocab = base_vocab.copy()
+        for merge in tqdm.tqdm(merges):
+            pair = merge.as_int_tuple()
+            ix = reverse_new_vocab[pair]
+            byts = pair2bytes(pair, base_vocab, new_vocab)
+            vocab[ix] = byts
+        return cls(vocab=vocab)
+
+    def convert_strings_to_int_tuples(
+        self, strs: List[str], special_tokens: List[str]
+    ) -> List[Tuple[int, ...]]:
+        int_tups = []
+        for s in strs:
+            if s in special_tokens:
+                idxs = [self._inverse_mapping[bytes(s, "utf-8")]]
+            else:
+                byte_list = [bytes([b]) for b in bytes(s, "utf-8")]
+                idxs = [self._inverse_mapping[byt] for byt in byte_list]
+            int_tups.append(idxs)
+        return int_tups
+
+    def save(self, path: str):
+        with open(path, "w", encoding="utf-8") as file:
+            json.dump(
+                {
+                    i: b64.b64encode(bs).decode("ascii")
+                    for i, bs in self._forward_mapping.items()
+                },
+                file,
+            )
+
+    @property
+    def forward_mapping(self) -> Dict[int, bytes]:
+        return self._forward_mapping
+
+    @property
+    def inverse_mapping(self) -> Dict[bytes, int]:
+        return self._inverse_mapping
 
 
 class PairIndex:
@@ -145,6 +252,8 @@ class PairIndex:
 
 class BPETokenizer:
 
+    BASE_VOCAB_SIZE = 256
+
     def __init__(
         self,
         vocab_size: int,
@@ -156,86 +265,7 @@ class BPETokenizer:
         self._vocab_size = vocab_size
         self._special_tokens = special_tokens or []
         self._pretok_regex = pretokenization_pattern
-
-        self._base_vocab = {i: bytes([i]) for i in range(256)}
-        for i, special_token in enumerate(self._special_tokens):
-            self._base_vocab[i + 256] = special_token.encode("utf-8")
-        self._base_vocab_size = len(self._base_vocab)
-        self._reverse_vocab = {v: k for k, v in self._base_vocab.items()}
-
-        self._new_vocab = {}
-        self._reverse_new_vocab = {}
-
-        self._merges = None
-        self._vocab = None
-        self._reverse_vocab = None
-
-    @classmethod
-    def from_vocab_and_merges(
-        cls,
-        vocab: Dict[int, bytes],
-        merges: List[Tuple[bytes, bytes]],
-        special_tokens: Optional[List[str]] = None,
-    ) -> "BPETokenizer":
-        tokenizer = cls(vocab_size=len(vocab), special_tokens=special_tokens)
-        tokenizer._merges = merges
-        tokenizer._vocab = vocab
-        tokenizer._reverse_vocab = {v: k for k, v in vocab.items()}
-        return tokenizer
-
-    def _int2bytes(self, ix: int) -> bytes:
-        if ix in self._base_vocab:
-            return self._base_vocab[ix]
-        ix_left, ix_right = self._new_vocab[ix]
-        return self._int2bytes(ix_left) + self._int2bytes(ix_right)
-
-    def _pair2bytes(self, pair: Tuple[int, int]) -> bytes:
-        return self._int2bytes(pair[0]) + self._int2bytes(pair[1])
-
-    def _assemble_byte_vocab(self, merges: List[Tuple[int, int]]) -> Dict[int, bytes]:
-        assert self._merges is not None
-        vocab = self._base_vocab.copy()
-        for pair in tqdm.tqdm(merges):
-            ix = self._reverse_new_vocab[pair]
-            byts = self._pair2bytes(pair)
-            vocab[ix] = byts
-        return vocab
-
-    @classmethod
-    def from_files(cls, vocab_path: str, merges_path: str) -> "BPETokenizer":
-        with open(vocab_path, "r", encoding="utf-8") as file:
-            vocab = {int(k): b64.b64decode(v) for k, v in json.load(file).items()}
-        with open(merges_path, "r", encoding="utf-8") as file:
-            merges = []
-            for line in file:
-                tup = tuple(line.strip().split("\t"))
-                merges.append((b64.b64decode(tup[0]), b64.b64decode(tup[1])))
-        return BPETokenizer.from_vocab_and_merges(vocab, merges)
-
-    @property
-    def vocab(self) -> Dict[int, bytes]:
-        if self._vocab is None:
-            raise ValueError("tokenizer must be trained to access vocab")
-        return self._vocab
-
-    def _assemble_byte_merges(
-        self, merges: List[Tuple[int, int]]
-    ) -> List[Tuple[bytes, bytes]]:
-        byte_merges = []
-        for p1, p2 in tqdm.tqdm(merges):
-            byte_merges.append((self._int2bytes(p1), self._int2bytes(p2)))
-        return byte_merges
-
-    @property
-    def merges(self) -> List[Tuple[bytes, bytes]]:
-        if self._merges is None:
-            raise ValueError("tokenizer must be trained to access merges")
-        return self._merges
-
-    def _pretokenize(self, text: str) -> List[str]:
-        if not any(stok in text for stok in self._special_tokens):
-            return re.findall(self._pretok_regex, text)
-        special_token_regex = (
+        self._special_token_regex = (
             "("
             + "|".join(
                 [
@@ -245,7 +275,55 @@ class BPETokenizer:
             )
             + ")"
         )
-        special_parts = re.split(special_token_regex, text)
+
+        self._base_vocab = {i: bytes([i]) for i in range(256)}
+        for i, special_token in enumerate(self._special_tokens):
+            self._base_vocab[i + 256] = special_token.encode("utf-8")
+
+        self._merges = None
+        self._vocab = None
+
+    @classmethod
+    def from_vocab_and_merges(
+        cls,
+        vocab: Dict[int, bytes],
+        merges: List[Tuple[bytes, bytes]],
+        special_tokens: Optional[List[str]] = None,
+    ) -> "BPETokenizer":
+        tokenizer = cls(vocab_size=len(vocab), special_tokens=special_tokens)
+        tokenizer._merges = [Merge.from_bytes(*byte_merge) for byte_merge in merges]
+        tokenizer._vocab = Vocab(vocab)
+        return tokenizer
+
+    @classmethod
+    def from_files(cls, vocab_path: str, merges_path: str) -> "BPETokenizer":
+        with open(merges_path, "r", encoding="utf-8") as file:
+            merges = []
+            for line in file:
+                tup = tuple(line.strip().split("\t"))
+                merges.append(
+                    Merge.from_bytes(b64.b64decode(tup[0]), b64.b64decode(tup[1]))
+                )
+        with open(vocab_path, "r", encoding="utf-8") as file:
+            vocab = {int(k): b64.b64decode(v) for k, v in json.load(file).items()}
+        return BPETokenizer.from_vocab_and_merges(vocab, merges)
+
+    @property
+    def vocab(self) -> Vocab:
+        if self._vocab is None:
+            raise ValueError("tokenizer must be trained to access vocab")
+        return self._vocab
+
+    @property
+    def merges(self) -> List[Merge]:
+        if self._merges is None:
+            raise ValueError("tokenizer must be trained to access merges")
+        return self._merges
+
+    def _pretokenize(self, text: str) -> List[str]:
+        if not any(stok in text for stok in self._special_tokens):
+            return re.findall(self._pretok_regex, text)
+        special_parts = re.split(self._special_token_regex, text)
         pretokens = []
         for part in special_parts:
             if part in self._special_tokens:
@@ -254,15 +332,22 @@ class BPETokenizer:
                 pretokens.extend(re.findall(self._pretok_regex, part))
         return pretokens
 
-    def _get_pretoken_counts(self, words: List[str]) -> Dict[Tuple, int]:
-        counts = collections.defaultdict(int)
-        for word in tqdm.tqdm(words):
-            byte_tuple = tuple(bytes(word, encoding="utf-8"))  # Tuple[int]
-            counts[byte_tuple] += 1
-        return counts
+    def _ipair2bpair(
+        self,
+        pair: Tuple[int, int],
+        base_vocab: Dict[int, bytes],
+        new_vocab: Dict[int, Tuple[int, int]],
+    ) -> Tuple[bytes, bytes]:
+        return (
+            int2bytes(pair[0], base_vocab, new_vocab),
+            int2bytes(pair[1], base_vocab, new_vocab),
+        )
 
     def _most_common_pair(
-        self, counts: Dict[Tuple[int, int], int]
+        self,
+        counts: Dict[Tuple[int, int], int],
+        base_vocab: Dict[int, bytes],
+        new_vocab: Dict[int, Tuple[int, int]],
     ) -> Tuple[Tuple[int, int], int]:
         max_count = -sys.maxsize
         most_frequent_pair = (-1, -1)
@@ -276,10 +361,9 @@ class BPETokenizer:
             if count == max_count:
                 # if the counts are equal, we have to resolve the pieces of the pair
                 # separately and compare them for lexical order
-                ip_pair = (self._int2bytes(pair[0]), self._int2bytes(pair[1]))
-                max_ip_pair = (
-                    self._int2bytes(most_frequent_pair[0]),
-                    self._int2bytes(most_frequent_pair[1]),
+                ip_pair = self._ipair2bpair(pair, base_vocab, new_vocab)
+                max_ip_pair = self._ipair2bpair(
+                    most_frequent_pair, base_vocab, new_vocab
                 )
                 if ip_pair > max_ip_pair:
                     max_count = count
@@ -293,71 +377,59 @@ class BPETokenizer:
             chunk_size=self._file_chunk_size,
             regex_pattern=self._pretok_regex,
         )
-        num_merges = self._vocab_size - self._base_vocab_size
-        merges_int = []
+        num_merges = self._vocab_size - BPETokenizer.BASE_VOCAB_SIZE
+        merges = []
+        base_vocab = {i: bytes([i]) for i in range(BPETokenizer.BASE_VOCAB_SIZE)}
+        new_vocab = {}
         for i in tqdm.tqdm(range(num_merges)):
             most_frequent_pair, most_frequent_count = self._most_common_pair(
-                pair_index.pair_counts
+                pair_index.pair_counts, base_vocab, new_vocab
             )
 
             if most_frequent_count == 1:
                 break
 
-            merges_int.append(most_frequent_pair)
+            merges.append(Merge.from_ints(*most_frequent_pair))
 
-            new_idx = self._base_vocab_size + i
-            self._new_vocab[new_idx] = most_frequent_pair
-            self._reverse_new_vocab[most_frequent_pair] = new_idx
+            new_idx = BPETokenizer.BASE_VOCAB_SIZE + i
+            new_vocab[new_idx] = most_frequent_pair
 
             pair_index.apply_merge(most_frequent_pair, new_idx)
 
-        self._merges = self._assemble_byte_merges(merges_int)
-        self._vocab = self._assemble_byte_vocab(merges_int)
-        self._reverse_vocab = {v: k for k, v in self._vocab.items()}
+        self._merges = merges
+        self._vocab = Vocab.from_base_and_new(base_vocab, new_vocab, merges)
 
     def save(self, vocab_path: str, merges_path: str):
         assert self._merges is not None
         assert self._vocab is not None
-        with open(vocab_path, "w", encoding="utf-8") as file:
-            json.dump(
-                {i: b64.b64encode(bs).decode("ascii") for i, bs in self._vocab.items()},
-                file,
-            )
+
+        self._vocab.save(vocab_path)
+
         with open(merges_path, "w", encoding="utf-8") as file:
             for merge in self._merges:
-                left = b64.b64encode(merge[0]).decode("ascii")
-                right = b64.b64encode(merge[1]).decode("ascii")
+                merge_byte_pair = merge.as_byte_tuple()
+                left = b64.b64encode(merge_byte_pair.left).decode("ascii")
+                right = b64.b64encode(merge_byte_pair.right).decode("ascii")
                 file.write(f"{left}\t{right}\n")
+
+    def _apply_all_merges(self, merges, token):
+        merged_token = token
+        for j, merge in enumerate(merges):
+            merged_token = apply_merge_to_word(merged_token, merge, 256 + j)
+        return merged_token
 
     def encode(self, text: str) -> List[int]:
         assert self._merges is not None
-        assert self._reverse_vocab is not None
-        str_pretokens = self._pretokenize(text)
-        pretokens = []
-        for i, pretoken in enumerate(str_pretokens):
-            if pretoken in self._special_tokens:
-                idxs = [self._reverse_vocab[bytes(pretoken, "utf-8")]]
-            else:
-                byte_list = [bytes([b]) for b in bytes(pretoken, "utf-8")]
-                idxs = [self._reverse_vocab[byt] for byt in byte_list]
-            pretokens.append(idxs)
+        assert self._vocab is not None
+        pretokens = self._vocab.convert_strings_to_int_tuples(
+            self._pretokenize(text), self._special_tokens
+        )
         int_merges = [
-            (
-                self._reverse_vocab[merge[0]],
-                self._reverse_vocab[merge[1]],
-            )
-            for merge in self._merges
+            merge.as_int_tuple(self._vocab.inverse_mapping) for merge in self._merges
         ]
         for i, pretoken in enumerate(pretokens):
-            merged_token = pretoken
-            if len(merged_token) > 1:
-                for j, merge in enumerate(int_merges):
-                    merged_token = apply_merge_to_word(merged_token, merge, 256 + j)
-            pretokens[i] = merged_token
-        ids = []
-        for merged_pretoken in pretokens:
-            ids.extend(merged_pretoken)
-        return ids
+            pretokens[i] = self._apply_all_merges(int_merges, pretoken)
+        return [id_ for merged_pretoken in pretokens for id_ in merged_pretoken]
 
     def encode_iterable(self, iterable: Iterable[str]):
         for s in iterable:
@@ -366,5 +438,4 @@ class BPETokenizer:
 
     def decode(self, ids: List[int]) -> str:
         assert self._vocab is not None
-        print(ids, [self._vocab[id_] for id_ in ids])
         return b"".join([self._vocab[id_] for id_ in ids]).decode("utf-8")
