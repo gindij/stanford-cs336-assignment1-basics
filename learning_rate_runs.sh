23b0ec96a3eedaeac2acffafdad7e720c0b120d4
gindije <gindije@gmail.com>
2026-01-01 11:50:34 -0500

    more lr experiments

--- learning_rate_runs.sh
+++ learning_rate_runs.sh
@@ -1,14 +1,16 @@
-python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 1000 --lr-cosine-cycle-iters 19000 --lr-min 0.00000001 --lr-max 0.0001 --checkpoint-dir lr10
+python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 1000 --lr-cosine-cycle-iters 19000 --lr-min 0.0000001 --lr-max 0.01 --checkpoint-dir lr10
 
-python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 500 --lr-cosine-cycle-iters 19500 --lr-min 0.00000001 --lr-max 0.0001 --checkpoint-dir lr9
+python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 500 --lr-cosine-cycle-iters 19500 --lr-min 0.0000001 --lr-max 0.01 --checkpoint-dir lr9
 
-python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 0 --lr-cosine-cycle-iters 20000 --lr-min 0.00000001 --lr-max 0.0001 --checkpoint-dir lr8
+python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 0 --lr-cosine-cycle-iters 20000 --lr-min 0.0000001 --lr-max 0.01 --checkpoint-dir lr8
+
+# python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 0 --lr-cosine-cycle-iters 20000 --lr-min 0.00000001 --lr-max 0.0001 --checkpoint-dir lr8
 
 # python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 1000 --lr-cosine-cycle-iters 18000 --lr-min 0.000001 --lr-max 0.001 --checkpoint-dir lr10
 
 # python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 500 --lr-cosine-cycle-iters 18500 --lr-min 0.000001 --lr-max 0.001 --checkpoint-dir lr9
 
-python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 0 --lr-cosine-cycle-iters 19000 --lr-min 0.000001 --lr-max 0.001 --checkpoint-dir lr8
+# python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 0 --lr-cosine-cycle-iters 19000 --lr-min 0.000001 --lr-max 0.001 --checkpoint-dir lr8
 
 # python -m cs336_basics.train_model --vocab-size 10000 --context-length 256 --d-model 512 --d-ff 2048 --num-layers 4 --num-heads 16 --epochs 100 --num-train-batches 200 --num-eval-batches 1000 --batch-size 64 --lr-warmup-iters 2000 --lr-cosine-cycle-iters 16000 --lr-min 0.000001 --lr-max 0.001 --checkpoint-dir lr7
 
